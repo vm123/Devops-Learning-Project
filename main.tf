a352419ed7dd0e019fc995f0b81e36418d5cb497
vm123 <39663352+vm123@users.noreply.github.com>
2022-06-14 20:35:37 -0500

    Update main.tf
    Signed-off-by: vm123 <39663352+vm123@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -45,3 +45,10 @@ resource "azurerm_mssql_database" "heart8rate" {
   name      = "heart8rate"
   server_id = "/subscriptions/5ec6ddd8-60a2-4186-9e32-59fbeab53734/resourceGroups/DataBase_Resource_Group/providers/Microsoft.Sql/servers/sqlserver84209"
 }
+
+resource "azurerm_mssql_firewall_rule" "firewallrule" {
+  name             = "firewallrule"
+  server_id        = "/subscriptions/5ec6ddd8-60a2-4186-9e32-59fbeab53734/resourceGroups/DataBase_Resource_Group/providers/Microsoft.Sql/servers/sqlserver84209"
+  start_ip_address = "167.96.157.56"
+  end_ip_address   = "192.168.0.14."
+}
